3c622038339f27bfb1c2359284a369a6be6dc216
Dasith Samarasinghe <89783432+Davids-98@users.noreply.github.com>
2022-09-19 18:43:11 +0530

    User profile intiated

--- src/pages/SellerProfile.jsx
+++ src/pages/SellerProfile.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {COLLECTION_DATA} from '../asssets/data/data.js'
+import NFTList from '../components/ui/NFTList/NFTList.jsx';
+import ProfileHead from '../components/ui/ProfileHead/ProfileHead'
 
 function SellerProfile() {
   return (
-    <div>SellerProfile</div>
+    <section>
+      <div>
+        <ProfileHead key={COLLECTION_DATA.collectionId} collectionData = {COLLECTION_DATA}/>
+        <NFTList/>
+      </div>
+    </section>
+
   )
 }
 
