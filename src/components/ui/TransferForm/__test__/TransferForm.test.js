316fd87951ba5117fbd3cecbf0a17b03c3daa15f
Dasith Samarasinghe <89783432+Davids-98@users.noreply.github.com>
2022-10-04 02:26:43 +0530

    final

--- src/components/ui/TransferForm/__test__/TransferForm.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import TransferForm from '../TransferForm';
-
-test('Should render input element', () => {
-  render(<TransferForm title="my header" />);
-  const inputElement = screen.getByPlaceholderText(/Enter Receiver's Wallet Address/i);
-  expect(inputElement).toBeInTheDocument();
-});
