88d9f3f30a848833184a5e1ee11cf774544e30b8
Thilina Sandakelum <88228790+thilinawaks1999@users.noreply.github.com>
2022-10-03 15:21:27 +0530

    Update EditAdminDetails.jsx

--- src/pages/Nadmin/EditAdminDetails.jsx
+++ src/pages/Nadmin/EditAdminDetails.jsx
@@ -1,14 +1,1 @@
-import React from "react";
-import "../../styles/editadmin.css";
 
-function EditAdminDetails() {
-  return (
-    <div className="main">
-      <div>
-        <h1>Edit Details of Admins</h1>
-      </div>
-    </div>
-  );
-}
-
-export default EditAdminDetails;
