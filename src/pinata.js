8ba39e931c42eff30e935018ee46206710d66c1b
dinushaAsamarakoon <dinusha.19@cse.mrt.ac.lk>
2022-11-22 14:17:24 +0530

    hardhat test

--- src/pinata.js
+++ src/pinata.js
@@ -1,12 +1,16 @@
 //require('dotenv').config();
 // const key = process.env.PINATA_API_KEY;
 // console.log('key: ',key);
-const key = "79e96f2c633a1d649657";
-const secret = "6ecb8befe4be3049083405b82eba49c2738ddae36cc3bce327ed7f52fe325b33";
+// const key = "79e96f2c633a1d649657";
+// const secret = "6ecb8befe4be3049083405b82eba49c2738ddae36cc3bce327ed7f52fe325b33";
 
 //second
 // const key = "d1ac1f06fc8c74bb0e77";
 // const secret = "5bda47bcfd583cf284f5a0f2aa183aa098da7b80691dbec2e7d2831c4e90f54c";
+
+//third
+const key = "a2873abf28086dddfadb";
+const secret = "93c198441ba978f62e1ffbcec04f65833807f54e5eedc761d31a83b1a280d0b3";
 
 // const secret = process.env.PIANTA_SECRET_KEY;
 // console.log('secret: ',secret);
